a1306b0a52ab0120424ec11f8535d3b9a68c96be
fragomez <fmgomez60@misena.edu.co>
2022-11-02 12:23:51 -0500

    Ejercicio02

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 addEventListener("DOMContentLoaded", (e) => {
-    let zodiacoForm = document.querySelector("#zodiacoForm");
-    zodiacoForm.addEventListener("submit", async(e) => {
+    let calcular = document.querySelector("#calcular");
+    calcular.addEventListener("submit", async(e) => {
         e.preventDefault();
         let datos = Object.fromEntries(new FormData(e.target));
         let config = {
-            method: zodiacoForm.method,
+            method: calcular.method,
             body: JSON.stringify(datos)
         };
-        let peticion = await fetch(zodiacoForm.action, config);
-        let resultado = await peticion.json();
-        document.querySelector("#resultado").innerHTML = resultado.mensaje;
+        let peticion = await fetch(calcular.action, config);
+        let resultado = await peticion.text();
+        document.querySelector("#resultado").innerHTML = resultado;
     })
 })
