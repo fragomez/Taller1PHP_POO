e73e4fb2da1d5aa0267b4e85669de73f47e71825
fragomez <fmgomez60@misena.edu.co>
2022-11-03 21:23:42 -0500

    Ejercicio03

--- api.php
+++ api.php
@@ -1,34 +1,64 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json; charset=utf-8');
-    $_DATA = json_decode(file_get_contents("php://input"),true);
-    class Factura{
-        public $codigo;
-        public $cantidad;
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
+$_DATA = json_decode(file_get_contents("php://input"), true);
+class Promedio
+{
+    public $sumaA;
+    public $cantidadA;
+    public $sumaB;
+    public $cantidadB;
+    public $sumaC;
+    public $cantidadC;
 
-        public function __construct(int $codigo, int $cantidad){
-            $this->codigo = $codigo;
-            $this->cantidad = $cantidad;
-        }
+    public function __construct(int $sumaA, int $cantidadA, int $sumaB, int $cantidadB, int $sumaC, int $cantidadC)
+    {
+        $this->sumaA = $sumaA;
+        $this->cantidadA = $cantidadA;
+        $this->sumaB = $sumaB;
+        $this->cantidadB = $cantidadB;
+        $this->sumaC = $sumaC;
+        $this->cantidadC = $cantidadC;
+    }
 
-        public function codigos(){
+    public function promedioFinal(): string
+    {
 
-            $codigo = match($this->codigo){
-                100 => 20000,
-                200 => 60000,
-                300 => 10000,
-                400 => 5000,
-                500 => 30000,
-                default => "Error: por favor ingrese un código correcto"
-            };
-            return $codigo;
-        }
-        public function pagar():string{
-            $codigo = $this->codigos();
-            $pagar = (gettype($codigo) == "string") ? $codigo : $codigo * $this->cantidad;
-            return $pagar;
+        $sumaA = $this->sumaA;
+        $cantidadA = $this->cantidadA;
+        $sumaB = $this->sumaB;
+        $cantidadB = $this->cantidadB;
+        $sumaC = $this->sumaC;
+        $cantidadC = $this->cantidadC;
+
+        $cantidadA < 1 ? $promedioA = 0 : $promedioA = $sumaA / $cantidadA;
+        $cantidadB < 1 ? $promedioB = 0 : $promedioB = $sumaB / $cantidadB;
+        $cantidadC < 1 ? $promedioC = 0 : $promedioC = $sumaC / $cantidadC;
+
+        if ($promedioA > $promedioB && $promedioA > $promedioC) {
+            if ($promedioB > $promedioC) {
+                return "C";
+            } else {
+                return "B";
+            }
+        } else if ($promedioB > $promedioA && $promedioB > $promedioC) {
+            if ($promedioA > $promedioC) {
+                return "C";
+            } else {
+                return "A";
+            }
+        } else if ($promedioC > $promedioA && $promedioC > $promedioB) {
+            if ($promedioA > $promedioB) {
+                return "B";
+            } else {
+                return "A";
+            }
+        } else {
+            return "A, B, C";
         }
     }
-    $factura = new Factura(codigo:$_DATA['codigo'],cantidad:$_DATA['cantidad']);
-    echo($factura->pagar());
+}
+
+$promedio = new Promedio(sumaA: $_DATA[0], cantidadA: $_DATA[1], sumaB: $_DATA[2], cantidadB: $_DATA[3], sumaC: $_DATA[4], cantidadC: $_DATA[5]);
+echo ($promedio->promedioFinal());
 ?>
--- main.js
+++ main.js
@@ -1,14 +1,79 @@
+let sumaA = 0, sumaB = 0, sumaC = 0, cantidadA = 0, cantidadB = 0, cantidadC = 0;
+
+function sumarPromedio(genero, carrera, edad) {
+    if (genero == "masculino") {
+        if (carrera == "a") {
+            sumaA += edad;
+            cantidadA += 1;
+        } else if (carrera == "b") {
+            sumaB += edad;
+            cantidadB += 1
+        } else {
+            sumaC += edad;
+            cantidadC += 1;
+        }
+    } else {
+        null
+    }
+}
 addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector("#cantidad").removeAttribute("disabled");
+    document.querySelector("#cantidad").addEventListener("keyup", (e) => {
+        let genero = document.querySelector("#genero");
+        let edad = document.querySelector("#edad");
+        let carrera = document.querySelector("#carrera");
+
+        if (e.target.value > 0) {
+            genero.removeAttribute("disabled");
+            edad.removeAttribute("disabled");
+            carrera.removeAttribute("disabled");
+        } else {
+            genero.disabled = true;
+            edad.disabled = true;
+            carrera.disabled = true;
+        }
+    })
+    let contador = 1;
+    let listaPromedio = [];
     let calcular = document.querySelector("#calcular");
-    calcular.addEventListener("submit", async(e) => {
+    calcular.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let datos = Object.fromEntries(new FormData(e.target));
-        let config = {
-            method: calcular.method,
-            body: JSON.stringify(datos)
-        };
-        let peticion = await fetch(calcular.action, config);
-        let resultado = await peticion.text();
-        document.querySelector("#resultado").innerHTML = resultado;
+        let cantidad = document.querySelector("#cantidad").value;
+        document.querySelector("#cantidad").disabled = true;
+        if (contador < cantidad) {
+            let datos = Object.fromEntries(new FormData(calcular));
+            sumarPromedio(datos["genero"], datos["carrera"], parseInt(datos["edad"]));
+            document.querySelector("#genero").selectedIndex = 0;
+            document.querySelector("#edad").value = "";
+            document.querySelector("#carrera").selectedIndex = 0;
+            contador += 1;
+        } else {
+            let calcular = e.target;
+            let datos = Object.fromEntries(new FormData(calcular));
+            sumarPromedio(datos["genero"], datos["carrera"], parseInt(datos["edad"]));
+            document.querySelector("#estudiantes").innerHTML = "";
+            listaPromedio.push(sumaA);
+            listaPromedio.push(cantidadA);
+            listaPromedio.push(sumaB);
+            listaPromedio.push(cantidadB);
+            listaPromedio.push(sumaC);
+            listaPromedio.push(cantidadC);
+
+            let config = {
+                method: calcular.method,
+                body: JSON.stringify(listaPromedio)
+            };
+
+            let peticion = await fetch(calcular.action, config);
+            let respuesta = await peticion.text();
+
+            document.querySelector("#estudiantes").insertAdjacentHTML("beforeend", respuesta);
+            document.querySelector("#genero").selectedIndex = 0;
+            document.querySelector("#edad").value = "";
+            document.querySelector("#carrera").selectedIndex = 0;
+            genero.disabled = true;
+            edad.disabled = true;
+            carrera.disabled = true;
+        }
     })
 })
